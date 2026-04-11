01f731e47e598a55fb58e221ac457b0fef246a84
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-04-11 04:55:16 -0300

    test: fixed bug in app test

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,23 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { PLATFORM_ID } from '@angular/core';
+
 import { App } from './app';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
+  let spectator: Spectator<App>;
+
+  const createComponent = createComponentFactory({
+    component: App,
+    providers: [
+      { provide: PLATFORM_ID, useValue: 'browser' },
+      provideRouter([])
+    ]
+  });
+
+  beforeEach(() => {
+    spectator = createComponent();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aquarismo-app');
+    expect(spectator.component).toBeTruthy();
   });
 });
