53df0586229503dc82f51d9f8082a3f074dd7955
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-04-13 13:31:56 -0300

    fix: fixed test of the post betta fish 7 care tips

--- frontend/src/app/features/pages/articles/betta-fish-7-care-tips/betta-fish-7-care-tips.spec.ts
+++ frontend/src/app/features/pages/articles/betta-fish-7-care-tips/betta-fish-7-care-tips.spec.ts
@@ -1,23 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { SeoService } from '../../../../core/services/seo/seo-service';
+import { PostService } from '../../../../core/services/post/post';
 import { BettaFish7CareTips } from './betta-fish-7-care-tips';
 
+
 describe('BettaFish7CareTips', () => {
-  let component: BettaFish7CareTips;
-  let fixture: ComponentFixture<BettaFish7CareTips>;
+  let spectator: Spectator<BettaFish7CareTips>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BettaFish7CareTips]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: BettaFish7CareTips,
+    providers: [
+      {
+        provide: SeoService,
+        useValue: { updateMetadata: jest.fn() }
+      },
+      {
+        provide: PostService,
+        useValue: {
+          getPostPageData: jest.fn().mockReturnValue({
+            post: {
+              title: 'Test',
+              description: 'Desc',
+              image: 'img.png',
+              slug: 'test'
+            },
+            recommended: [],
+            latest: []
+          })
+        }
+      }
+    ]
+  });
 
-    fixture = TestBed.createComponent(BettaFish7CareTips);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    spectator = createComponent();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
